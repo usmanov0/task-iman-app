25def974ea0d598630bba3094a499f646fadd215
usmanov0 <azikusmanov882@gmail.com>
2024-01-04 20:11:41 +0500

    deleted

--- internal/collection_service/domain/post_domain.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type Post struct {
-	Id     int    `json:"id"`
-	UserId int    `json:"userId"`
-	Title  string `json:"title"`
-	Body   string `json:"body"`
-}
--- internal/collection_service/domain/repository.go
+++
@@ -1,5 +0,0 @@
-package domain
-
-type PostRepository interface {
-	Save(post *Post) (int, error)
-}
