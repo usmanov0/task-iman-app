146ebb73bb3743ceb60bdf232a3744d7c435dbab
usmanov0 <azikusmanov882@gmail.com>
2024-01-09 10:23:44 +0500

    id and userId changed to int64

--- internal/post-crud-service/domain/post_domain.go
+++ internal/post-crud-service/domain/post_domain.go
@@ -1,8 +1,8 @@
 package domain
 
 type Post struct {
-	Id     int    `json:"id"`
-	UserId int    `json:"userId"`
+	Id     int64  `json:"id"`
+	UserId int64  `json:"userId"`
 	Title  string `json:"title"`
 	Body   string `json:"body"`
 }
