e43a456d5fe1e47b83a73a9ab3ba9e5d5be0827a
usmanov0 <azikusmanov882@gmail.com>
2024-01-11 17:07:04 +0500

    modified :domain

--- internal/post-crud-service/domain/repository.go
+++ internal/post-crud-service/domain/repository.go
@@ -1,7 +1,7 @@
 package domain
 
 type PostCrudRepository interface {
-	GetList() ([]Post, error)
+	GetList(page, limit int) ([]Post, error)
 	GetOne(postId int) (*Post, error)
 	Update(postId int, title, body string) (*PostUpdateResponse, error)
 	Delete(postId int) error
