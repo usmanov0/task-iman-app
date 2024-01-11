9dfb89a94a77f82507307814b6587f1c9eaa9068
usmanov0 <azikusmanov882@gmail.com>
2024-01-11 17:07:33 +0500

    added :CLI

---
+++ cmd/app/root.go
@@ -0,0 +1,50 @@
+package app
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	server2 "test-project-iman/cmd/app/server"
+)
+
+var root = &cobra.Command{Use: "run-grpc"}
+
+var postCollectorCmd = &cobra.Command{
+	Use: "grpc-server1",
+	Run: func(cmd *cobra.Command, args []string) {
+		server2.RunGrpcCollectorServer()
+	},
+}
+
+var postGrpcServerCmd = &cobra.Command{
+	Use: "grpc-server2",
+	Run: func(cmd *cobra.Command, args []string) {
+		server2.RunGrpcServer()
+	},
+}
+
+var apiGateway = &cobra.Command{
+	Use: "api-gateway",
+	Run: func(cmd *cobra.Command, args []string) {
+		server2.ApiGatewayServer()
+	},
+}
+
+func Execute() {
+	root.AddCommand(postCollectorCmd)
+	root.AddCommand(postGrpcServerCmd)
+	root.AddCommand(apiGateway)
+	if err := root.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "error while executing CLI %s", err)
+		os.Exit(1)
+	}
+}
+
+//var runAllCmd = &cobra.Command{
+//	Use:   "run-all",
+//	Short: "Run both Execute and ExecuteCollector",
+//	Run: func(cmd *cobra.Command, args []string) {
+//		Execute()
+//		ExecuteCollector()
+//	},
+//}
