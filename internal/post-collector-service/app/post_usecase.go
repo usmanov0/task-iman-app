4360da9cc0783db76eb417bc5161a386f3460f71
usmanov0 <azikusmanov882@gmail.com>
2024-01-12 16:38:32 +0500

    modified :usecase

--- internal/post-collector-service/app/post_usecase.go
+++ internal/post-collector-service/app/post_usecase.go
@@ -53,7 +53,7 @@ func (uc *PostCollectorService) CollectPosts() error {
 	for _, post := range allPosts {
 		_, err := uc.postRepo.Save(&post)
 		if err != nil {
-			return errors.New("Failed to save posts to the database. Please check your network connection and try again later.")
+			return errors.New("Failed to save posts to the database")
 		}
 	}
 	return nil
